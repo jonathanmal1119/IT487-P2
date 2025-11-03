9332eb0b377f966e3147257c536eb720f08fde1b
Jonathan Malave <jmaxmalave@aol.com>
2025-11-03 17:40:43 -0500

    Restart Screen Created.

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
