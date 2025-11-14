48f01c35b562970eefe84f02714cd30ae2537846
Dyvinia <dulana57@gmail.com>
2025-11-14 18:34:20 -0500

    add utils

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public static class Utils
+	{
+        public static T? N<T>(this T obj) where T : Object => obj == null ? null : obj;
+        public static float Round(this float num) => Mathf.Round(num);
+        public static Vector3 Round(this Vector3 vec) => new(vec.x.Round(), vec.y.Round(), vec.z);
+    }
+}
