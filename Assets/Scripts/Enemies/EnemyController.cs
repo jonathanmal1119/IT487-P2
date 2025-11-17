40ee0f73151481ae84378ca0bc7c184f3e94a479
rks32 <rks32@njit.edu>
2025-11-16 19:16:29 -0500

    Added animations for Zombie enemies

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,12 +8,18 @@ public class EnemyController : MonoBehaviour
 {
     public int health = 1000;
     public float movementSpeed = 5;
+    public float idleSpeed = 2;
     public int detectionRange;
     public int attackRange = 3;
     public int attackAmt = 10;
     public float attackTime = 1.5f;
+    public bool isDead = false;
 
     GameObject Player;
+
+    public Animator animator;
+    public RuntimeAnimatorController death;
+    public RuntimeAnimatorController attack;
 
     public bool playerDetected = false;
     bool attacking = false;
@@ -23,45 +30,76 @@ public class EnemyController : MonoBehaviour
 
     bool lostPlayer = false;
 
+    float randDirectionX = 0f;
+    float randDirectionZ = 0f;
+    bool isChangingDirection = false;
+
     Rigidbody rb;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         Player = GameObject.FindWithTag("Player");
+        animator = GetComponentInChildren<Animator>();
     }
 
     void FixedUpdate()
     {
-        if (health <= 0)
-            Destroy(this.gameObject);
+        Vector3 direction;
+        Vector3 newPosition;
+        Quaternion rotation;
 
-        distanceToPlayer = Vector3.Distance(rb.position, Player.transform.position);
-
-        if (distanceToPlayer <= detectionRange)
+        if (!isDead)
         {
-            playerDetected = true;
-            lostPlayer = false;
-        }
+            distanceToPlayer = Vector3.Distance(rb.position, Player.transform.position);
 
-        if (playerDetected == true && distanceToPlayer > detectionRange && lostPlayer == false)
-        {
-            StartCoroutine(outsideDetRad());
-            lostPlayer = true;
-        }
+            if (distanceToPlayer <= detectionRange)
+            {
+                playerDetected = true;
+                lostPlayer = false;
+            }
 
-        if (playerDetected == true)
-        {
-            Vector3 direction = (Player.transform.position - rb.position).normalized;
-            Vector3 newPosition = rb.position + direction * movementSpeed * Time.fixedDeltaTime;
+            if (playerDetected == true && distanceToPlayer > detectionRange && lostPlayer == false)
+            {
+                StartCoroutine(outsideDetRad());
+                lostPlayer = true;
+            }
 
-            rb.MovePosition(newPosition);
+            if (playerDetected == true)
+            {
+                direction = (Player.transform.position - rb.position).normalized;
+                newPosition = rb.position + direction * movementSpeed * Time.fixedDeltaTime;
 
-            transform.LookAt(Player.transform.position);
-        }
+                rb.MovePosition(newPosition);
 
-        if (Vector3.Distance(rb.position, Player.transform.position) <= attackRange && !attacking)
-        {
-            StartCoroutine(DamageCoroutine());
+                transform.LookAt(Player.transform.position);
+            }
+
+            if (Vector3.Distance(rb.position, Player.transform.position) <= attackRange && !attacking)
+            {
+                StartCoroutine(DamageCoroutine());
+            }
+
+            if (playerDetected == false)
+            {
+                direction = new Vector3(randDirectionX, 0, randDirectionZ);
+                newPosition = rb.position + direction * idleSpeed * Time.fixedDeltaTime;
+
+                if (direction != Vector3.zero)
+                {
+                    rotation = Quaternion.LookRotation(direction);
+                    rb.MoveRotation(rotation);
+                }
+
+                rb.MovePosition(newPosition);
+                if (isChangingDirection == false)
+                {
+                    StartCoroutine(changeDirection());
+                }
+            }
+
+            if (this.rb.position.y < -100)
+                StartCoroutine(Death());
+
         }
     }
 
@@ -72,12 +110,19 @@ public class EnemyController : MonoBehaviour
         while (Vector3.Distance(rb.position, Player.transform.position) <= attackRange)
         {
             Player.GetComponent<PlayerHealth>().TakeDamage(attackAmt);
+            animator.runtimeAnimatorController = attack;
             yield return new WaitForSeconds(attackTime);
         }
 
         attacking = false;
     }
 
+    IEnumerator Death()
+    {
+        animator.runtimeAnimatorController = death;
+        yield return new WaitForSeconds(1f);
+        Destroy(this.gameObject);
+    }
 
     public void TakeDamage(int Amt)
     {
@@ -91,7 +136,8 @@ public class EnemyController : MonoBehaviour
                     Instantiate(drops[selection], rb.transform.position, this.transform.rotation);
             }
             
-            Destroy(this.gameObject);
+            isDead = true;
+            StartCoroutine(Death());
         }
 
         health -= Amt;
@@ -135,6 +181,18 @@ public class EnemyController : MonoBehaviour
     {
             yield return new WaitForSeconds(7f);
             playerDetected = false;
+    }
+
+    private IEnumerator changeDirection()
+    {
+        isChangingDirection = true;
+
+        int delay = Random.Range(0, 7);
+        yield return new WaitForSeconds(delay);
+        randDirectionX = Random.Range(-2, 2);
+        randDirectionZ = Random.Range(-2, 2);
+
+        isChangingDirection = false;
     }
 
     private void OnTriggerEnter(Collider other)
