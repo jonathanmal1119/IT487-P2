c8ba99ecbd87658e4dd1ac6b06a728c577cc90c2
Dyvinia <dulana57@gmail.com>
2025-11-19 15:41:57 -0500

    font

--- Assets/Scripts/UI/ButtonHover.cs
+++ Assets/Scripts/UI/ButtonHover.cs
@@ -8,22 +8,31 @@ using UnityEngine.EventSystems;
 public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Color hoverColor = new(1, 1, 1, 1);
+    public float transitionSpeed = 15;
 
     TextMeshProUGUI text;
     Color originalColor;
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        text.color = hoverColor;
-    }
+    bool hovering = false;
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        text.color = originalColor;
-    }
     void Start()
     {
         text = GetComponentsInChildren<TextMeshProUGUI>().First();
         originalColor = text.color;
     }
+
+    public void Update()
+    {
+        if (hovering)
+        {
+            text.color = Color.Lerp(text.color, hoverColor, Time.deltaTime * transitionSpeed);
+        }
+        else
+        {
+            text.color = Color.Lerp(text.color, originalColor, Time.deltaTime * transitionSpeed);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) => hovering = true;
+    public void OnPointerExit(PointerEventData eventData) => hovering = false;
 }
