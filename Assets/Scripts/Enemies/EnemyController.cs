1eb280cc548682e8b99ea40b57995e36dea32330
rks32 <rks32@njit.edu>
2025-11-24 19:21:05 -0500

    Added stuff for the enemies

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -1,15 +1,23 @@
 using Assets.Scripts;
 using System.Collections;
+using Unity.VisualScripting;
+
 //using Unity.VisualScripting;
 //using UnityEditor.Experimental.GraphView;
 //using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.WSA;
+using UnityEngine.Audio;
 //using UnityEngine.Rendering;
 //using static UnityEngine.UI.GridLayoutGroup;
 
 public class EnemyController : MonoBehaviour
 {
-    public int health = 1000;
+    public bool isRangedEnemy;
+
+    public float health = 100;
     public float movementSpeed = 5;
     public float idleSpeed = 2;
     public int detectionRange;
@@ -26,6 +34,10 @@ public class EnemyController : MonoBehaviour
     public RuntimeAnimatorController walk;
     public RuntimeAnimatorController death;
     public RuntimeAnimatorController attack;
+    public RuntimeAnimatorController takeDamage;
+    public RuntimeAnimatorController idle;
+    public RuntimeAnimatorController aim;
+    bool isFlinching = false;
 
     public bool playerDetected = false;
     bool attacking = false;
@@ -43,12 +55,27 @@ public class EnemyController : MonoBehaviour
     Rigidbody rb;
 
     bool isPlayingSound = false;
+
+    public Slider HealthSlider;
+
+    public GameObject head;
+
+    public GameObject model;
+    public GameObject bodyExplode;
+
+    public bool returningToStart = false;
+    Vector3 Startpos;
+
+    public GameObject HeadshotEffect;
+    public bool isHeadshot = false;
+    public AudioClip headshotSoundEffect;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         Player = GameObject.FindWithTag("Player");
         animator = GetComponentInChildren<Animator>();
         audio = GetComponent<AudioSource>();
+        Startpos = this.transform.position;
     }
 
     void FixedUpdate()
@@ -75,6 +102,9 @@ public class EnemyController : MonoBehaviour
 
             if (playerDetected == true)
             {
+                if (isRangedEnemy)
+                    animator.runtimeAnimatorController = aim;
+
                 direction = (Player.transform.position - rb.position).normalized;
                 newPosition = rb.position + direction * movementSpeed * Time.fixedDeltaTime;
 
@@ -89,23 +119,61 @@ public class EnemyController : MonoBehaviour
                 StartCoroutine(DamageCoroutine());
             }
 
-            if (playerDetected == false)
-            {
-                direction = new Vector3(randDirectionX, 0, randDirectionZ);
-                newPosition = rb.position + direction * idleSpeed * Time.fixedDeltaTime;
 
-                if (direction != Vector3.zero)
+
+            if (!playerDetected)
+            {
+                float wanderDistance = Vector3.Distance(rb.position, Startpos);
+
+                if (!returningToStart)
                 {
+                    direction = new Vector3(randDirectionX, 0, randDirectionZ);
+
                     rotation = Quaternion.LookRotation(direction);
                     rb.MoveRotation(rotation);
+
+                    newPosition = rb.position + direction * idleSpeed * Time.fixedDeltaTime;
+
+                    rb.MovePosition(newPosition);
+                }
+                
+                if (wanderDistance >= 10 && returningToStart == false)
+                {
+                    //returningToStart = true;
+
+                    direction = (Startpos - transform.position).normalized;
+
+                    transform.LookAt(direction);
+
+                    newPosition = rb.position + direction * idleSpeed * Time.fixedDeltaTime;
+
+                    rb.MovePosition(newPosition);                    
                 }
 
-                rb.MovePosition(newPosition);
+                if (Vector3.Distance(rb.position, Startpos) < 2)
+                {
+                    returningToStart = false;
+                }
+
                 if (isChangingDirection == false)
                 {
                     StartCoroutine(changeDirection());
                 }
             }
+
+            /*if (!playerDetected && Vector3.Distance(Startpos, this.transform.position) >= 10 && !returningToStart)
+            {
+                returningToStart = true;
+                direction = (Startpos - this.transform.position).normalized;
+                rotation = Quaternion.LookRotation(direction);
+                rb.MoveRotation(rotation);
+                newPosition = rb.position + direction * idleSpeed * Time.fixedDeltaTime;
+
+                rb.MovePosition(newPosition);
+            }
+
+            if (Vector3.Distance(this.transform.position, Startpos) < 9)
+                returningToStart = false;*/
 
             if (this.rb.position.y < -100)
                 StartCoroutine(Death());
@@ -115,9 +183,17 @@ public class EnemyController : MonoBehaviour
                 isPlayingSound = true;
                 StartCoroutine(playSound());
             }
-        }
-    }
 
+            /*if (rb.angularVelocity == new Vector3(0, 0, 0))
+                animator.runtimeAnimatorController = idle;
+            else
+                animator.runtimeAnimatorController = walk;*/
+        }
+        if (HealthSlider != null)
+            HealthSlider.value = health;
+
+        HealthSlider.transform.LookAt(Player.transform.position);
+    }
     IEnumerator playSound()
     {
         int wait = Random.Range(0, 10);
@@ -125,6 +201,15 @@ public class EnemyController : MonoBehaviour
         audio.Play();
         yield return new WaitForSeconds(wait);
         isPlayingSound = false;
+    }
+
+    IEnumerator lowerVolume()
+    {
+        while (audio.volume > 0)
+        {
+            audio.volume -= 0.01f;
+            yield return new WaitForSeconds(0.015f);
+        }
     }
 
     IEnumerator DamageCoroutine()
@@ -142,14 +227,36 @@ public class EnemyController : MonoBehaviour
 
     IEnumerator Death()
     {
+        if (isHeadshot == true)
+        {
+            Instantiate(HeadshotEffect, head.transform.position, transform.rotation);
+            audio.clip = headshotSoundEffect;
+            audio.Play();
+            StartCoroutine(lowerVolume());
+            Debug.Log("HeadSHOT KILL");
+        }
         animator.runtimeAnimatorController = death;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.8f);
+        model.SetActive(false);
+        bodyExplode.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
         Destroy(this.gameObject);
     }
 
-    public void TakeDamage(int Amt, out bool killed)
+    IEnumerator flinch()
+    {
+        animator.runtimeAnimatorController = takeDamage;
+        yield return new WaitForSeconds(1f);
+        if (health > 0)
+            animator.runtimeAnimatorController = walk;
+    }
+
+    public void TakeDamage(float Amt, out bool killed)
     {
         killed = false;
+
+        if (!killed)
+            StartCoroutine(flinch());
 
         if (health - Amt <= 0)
         {
@@ -169,6 +276,9 @@ public class EnemyController : MonoBehaviour
             StartCoroutine(Death());
         }
 
+        if (!killed)
+            isHeadshot = false;
+        
         health -= Amt;
     }
 
@@ -181,10 +291,24 @@ public class EnemyController : MonoBehaviour
             //tries to find the bullet's PlayerBullet script, which contains information on how much damage it deals
             PlayerBullet? bulletInfo = collision.gameObject.GetComponent<PlayerBullet>().N();
 
+            float distanceToHead = Vector3.Distance(bulletInfo.transform.position, head.transform.position);
+            float damage = bulletInfo.damage;
+
             //If this scipt found the bullet's script, it can deal the proper amount of damage. Otherwise it will just deal 10.
             if (bulletInfo?.waitingToDestroy == false)
             {
-                TakeDamage(bulletInfo.damage, out bool killed);
+                if (distanceToHead <= 0.5)
+                {
+                    Debug.Log("HEADSHOT");
+                    damage = bulletInfo.damage * 2.0f;
+                    isHeadshot = true;
+                }
+                else
+                {
+                    damage = bulletInfo.damage;
+                }
+
+                TakeDamage(damage, out bool killed);
 
                 // show kill marker if hit was a kill, otherwise show hit marker if still alive
                 if (killed)
@@ -212,9 +336,22 @@ public class EnemyController : MonoBehaviour
                 return;
 
             TakeDamage(100, out _);
+            StartCoroutine(launchCorpse());
         }
     }
 
+    IEnumerator launchCorpse()
+    {
+        Vector3 launchPos = new Vector3(this.transform.position.x, this.transform.position.y + 1.5f, this.transform.position.z);
+
+        while (this.transform.position != launchPos)
+        {
+            Vector3 launchDirection = new Vector3(0, 1, 0);
+            Vector3 launch = rb.position + launchDirection * movementSpeed * Time.deltaTime;
+            rb.MovePosition(launch);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
 
     //Enemy loses focus on Player when player is far enough away
     private IEnumerator outsideDetRad()
@@ -229,8 +366,10 @@ public class EnemyController : MonoBehaviour
 
         int delay = Random.Range(0, 7);
         yield return new WaitForSeconds(delay);
-        randDirectionX = Random.Range(-2, 2);
-        randDirectionZ = Random.Range(-2, 2);
+        do {
+            randDirectionX = Random.Range(-2, 2);
+            randDirectionZ = Random.Range(-2, 2);
+        } while (randDirectionX == 0 && randDirectionZ == 0);
 
         isChangingDirection = false;
     }
