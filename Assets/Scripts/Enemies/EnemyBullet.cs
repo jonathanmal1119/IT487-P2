8ce916db16c013fcdf8d67063768b0c897eb6c3b
Dyvinia <dulana57@gmail.com>
2025-12-07 20:09:44 -0500

    shockwave fx and fix enemy bullet fx

--- Assets/Scripts/Enemies/EnemyBullet.cs
+++ Assets/Scripts/Enemies/EnemyBullet.cs
@@ -10,7 +10,10 @@ public class EnemyBullet : MonoBehaviour
     void Start()
     {
         rb.AddForce(transform.forward * launchForce, ForceMode.Impulse);
-        Destroy(gameObject, lifetime);
+        //Destroy(gameObject, lifetime);
+
+        Invoke(nameof(DisableBullet), lifetime); // disable bullet based on lifetime
+        Destroy(gameObject, lifetime + 5); // keep object alive after disabling so the vfx can finish playing before destroying
     }
 
     private void OnTriggerEnter(Collider other)
@@ -20,5 +23,17 @@ public class EnemyBullet : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         OnTriggerEnter(collision.collider);
+    }
+
+    public void DisableBullet()
+    {
+        GetComponent<Collider>().enabled = false;
+        rb.isKinematic = false;
+        rb.linearVelocity = Vector3.zero;
+
+        foreach (MeshRenderer meshRenderer in GetComponentsInChildren<MeshRenderer>())
+            meshRenderer.enabled = false;
+        foreach (ParticleSystem particleSystem in GetComponentsInChildren<ParticleSystem>())
+            particleSystem.Stop();
     }
 }
